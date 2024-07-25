2b6f740d375a286c316a5caf1118c4b2b85b6f6e
cinothejk <marco.diblasi@external.stellantis.com>
2024-07-25 10:05:02 +0200

    Spring Boot 3.0.0 tutorial (part 03) - CRUD controller

--- SpringBootTutorial/src/main/java/com/cino/SpringBootTutorial/controller/CrudController.java
+++ SpringBootTutorial/src/main/java/com/cino/SpringBootTutorial/controller/CrudController.java
@@ -1,11 +1,12 @@
 package com.cino.SpringBootTutorial.controller;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,60 +14,45 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cino.SpringBootTutorial.dto.User;
+import com.cino.SpringBootTutorial.service.UserService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/crud")
-public class Crudcontroller {
+@RequiredArgsConstructor
+public class CrudController {
 
-private static List<User> users = new ArrayList<>();
+	private final UserService userService = new UserService();
 	
 	@GetMapping("")
 	public List<User> getUsers() {
-		return users;
+		return userService.getUsers();
+	}
+	
+	@GetMapping("/{id}")
+	public User getUserById(@PathVariable Integer id) {
+		return userService.getUserById(id);
 	}
 	
 	@PostMapping("")
 	public List<User> createUser(@RequestBody User user) {
-		List<User> usersTemp = new ArrayList<>(users);
-		usersTemp.add(user);
-		users = usersTemp;
-		return users;
+		return userService.createUser(user);
 	}
 	
 	@PutMapping("")
 	public List<User> updateUser(@RequestBody User user) {
-		List<User> usersTemp = users.stream()
-				.map((u) -> {
-					if (u.id() == user.id()) {
-						return u = new User(user.id(), user.firstname(), user.lastname());
-					}
-					return u;
-					})
-				.toList();
-		users = usersTemp;
-		return users;
+		return userService.updateUser(user);
 	}
 	
 	@PatchMapping("")
 	public List<User> patchUser(@RequestBody User user) {
-		List<User> usersTemp = users.stream()
-				.map((u) -> {
-					if (u.id() == user.id()) {
-						return u = new User(user.id(), user.firstname(), user.lastname());
-					}
-					return u;
-					})
-				.toList();
-		users = usersTemp;
-		return users;
+		return userService.patchUser(user);
 	}
 	
 	@DeleteMapping("")
 	public void deleteUser(@RequestBody User user) {
-		List<User> userTemp = users.stream()
-			.filter(u -> u.id() != user.id())
-			.toList();
-		users = userTemp;
+		userService.deleteUser(user);
 	}
 	
 }
